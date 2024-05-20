5c0df31cd3487dc0ca9da85754092966999af6cb
Mohan Krishna <mohankrishna8595@gmail.com>
2024-05-20 18:21:41 +0530

    add to git

---
+++ NumberProgram13/src/main/java/NumberProgram13.java
@@ -0,0 +1,29 @@
+
+public class NumberProgram13 
+{
+	public static void isArmstrongNumber(int n)
+	{
+		String str = n+"";
+		int exp = str.length();
+		int rem=0;
+		int sum=0;
+		int temp=n;
+		while(n>0)
+		{
+			rem=n%10;
+			sum=sum+(int)Math.pow(rem, exp);
+			n=n/10;
+		}
+
+		if(temp==sum)
+		{
+			System.out.println("The number "+temp+" is a Armstrong Number");
+		}
+		else
+		{
+			System.out.println("The number "+temp+" is a not Armstrong Number");
+		}
+
+
+	}
+}
---
+++ NumberProgram13/src/test/java/Program13Test.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Program13Test {
+
+	@Test
+	public void test() {
+		NumberProgram13 np = new NumberProgram13();
+				np.isArmstrongNumber(153);
+	}
+
+}
