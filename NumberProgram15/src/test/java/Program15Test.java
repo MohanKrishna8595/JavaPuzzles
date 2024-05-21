1640ee98935e9fc4128d9d93993fe6565d589a06
Mohan Krishna <mohankrishna8595@gmail.com>
2024-05-21 22:00:37 +0530

    add to git

---
+++ NumberProgram15/src/test/java/Program15Test.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Program15Test {
+
+	@Test
+	public void test() {
+		NumberProgram15 np = new NumberProgram15();
+		np.isGcd(5,10);
+	}
+
+}
