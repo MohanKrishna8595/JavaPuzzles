bf3f55a0b20562fb3c8424936a46d0efbfc130fd
Mohan Krishna <mohankrishna8595@gmail.com>
2024-05-22 14:35:32 +0530

    add to git

---
+++ NumberProgram17/src/main/java/NumberProgram17.java
@@ -0,0 +1,11 @@
+
+public class NumberProgram17 
+{
+	public static void isSquarerootOfnumber(int n)
+	{
+		int res=(int)Math.sqrt(n);
+		System.out.println(res);
+		
+	}
+
+}
---
+++ NumberProgram17/src/test/java/Program17Test.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Program17Test {
+
+	@Test
+	public void test() {
+		NumberProgram17 np = new NumberProgram17();
+		np.isSquarerootOfnumber(36);
+		
+	}
+
+}
