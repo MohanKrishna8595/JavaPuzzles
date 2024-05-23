b9ef370c6bb3ec57e6360b5528439baaeb19efd8
Mohan Krishna <mohankrishna8595@gmail.com>
2024-05-23 17:41:16 +0530

    add to git

---
+++ HarshadNumber/src/main/java/HarshadNumber.java
@@ -0,0 +1,25 @@
+
+public class HarshadNumber 
+{
+	public void isharshad(int n)
+	{
+		int sum=0,rem=0,temp=0;
+		for(int i=1;i<=n;i++)
+		{
+			sum=0;
+			rem=0;
+			temp=i;
+			while(i>0)
+			{
+				rem=i%10;
+				sum=sum+rem;
+				i=i/10;
+			}
+			if(temp%sum==0)
+			{
+				System.out.print(temp+" ");
+			}
+			i=temp;
+		}
+	}
+}
---
+++ HarshadNumber/src/test/java/HarshadNumberTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HarshadNumberTest {
+
+	@Test
+	public void test() {
+	 HarshadNumber np = new HarshadNumber();
+	 np.isharshad(100);
+	}
+
+}
