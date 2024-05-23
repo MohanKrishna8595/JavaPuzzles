b7967d1f94799c5df16879452335ce919542b109
Mohan Krishna <mohankrishna8595@gmail.com>
2024-05-23 13:08:40 +0530

    add to git

---
+++ NumberProgram19/src/main/java/NumberProgram19.java
@@ -0,0 +1,25 @@
+
+public class NumberProgram19 {
+	public static void isAutomorphicNumber(int n)
+	{
+		int temp=n;
+		int square=n*n;
+		while(n>0)
+		{
+			if(n%10!=square%10)
+			{
+				System.out.println("The number "+temp+" is not automorphic number");
+				return;
+			}
+			else
+			{
+				n = n/10;   
+				square = square/10;
+			}
+		}
+		System.out.println("The number "+temp+" is automorphic number");
+		return;
+	}
+
+}
+
---
+++ NumberProgram19/src/test/java/Number19Test.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Number19Test {
+
+	@Test
+	public void test() {
+		NumberProgram19 np = new NumberProgram19();
+		np.isAutomorphicNumber(76);
+	}
+
+}
