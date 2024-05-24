470424079cf7447429e62992e10e7a01b079c397
Mohan Krishna <mohankrishna8595@gmail.com>
2024-05-24 22:55:19 +0530

    add to git

---
+++ AutomorphicNumber/src/test/java/AutomorphicNumberTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AutomorphicNumberTest {
+
+	@Test
+	public void test() {
+		AutomorphicNumber np = new AutomorphicNumber();
+		np.isAutomorphicNumber(76);
+	}
+
+}
