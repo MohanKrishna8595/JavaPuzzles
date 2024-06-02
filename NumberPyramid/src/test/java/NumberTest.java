1b14d6f13e48dd9e3ca4bf91da38b05a060a9909
Mohan Krishna <mohankrishna8595@gmail.com>
2024-06-02 16:12:34 +0530

    add to git

---
+++ NumberPyramid/src/test/java/NumberTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class NumberTest {
+
+	@Test
+	public void test() {
+		NumberPyramid np=new NumberPyramid();
+		np.isNumber(4);
+	}
+
+}
---
+++ NumberTriangle/src/test/java/NumberTest.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class NumberTest {
+
+	@Test
+	public void test() 
+	{
+		NumberTriangle np = new NumberTriangle();
+		 np.isNumber(4);
+		
+	}
+
+}
