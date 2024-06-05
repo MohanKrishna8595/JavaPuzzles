3a6b159a5f6e633a483182a869bf97b43fcf0e26
Mohan Krishna <mohankrishna8595@gmail.com>
2024-06-05 18:06:07 +0530

    add to git

---
+++ SortingAscending/src/main/java/SortingAscending.java
@@ -0,0 +1,31 @@
+
+public class SortingAscending {
+	public void isSorting(int arr[])
+	{
+		System.out.print("Before Sorting : ");
+	   for(int i=0;i<=arr.length-1;i++)
+	   {
+		   System.out.print(arr[i]+" ");
+	   }
+	   for(int i=0;i<=arr.length-2;i++)
+	   {
+		   for(int j=0;j<=arr.length-2;j++)
+		   {
+			   if(arr[j]>arr[j+1])
+			   {
+				   int temp=arr[j];
+				   arr[j]=arr[j+1];
+				   arr[j+1]=temp;
+			   }
+		   }
+	   }
+	   System.out.print("\nAfter  Sorting : ");
+	   for(int i=0;i<=arr.length-1;i++)
+	   {
+		   System.out.print(arr[i]+" ");
+	   }
+	   
+		
+	}
+
+}
---
+++ SortingAscending/src/test/java/SortingTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SortingTest {
+
+	@Test
+	public void test() {
+		SortingAscending sd=new SortingAscending();
+		int arr [] = {8,6,5,7};
+		sd.isSorting(arr);
+	}
+
+}
