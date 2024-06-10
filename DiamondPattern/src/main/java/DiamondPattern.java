8f739774d299d919907b9a8492723030a2dc093b
Mohan Krishna <mohankrishna8595@gmail.com>
2024-06-10 10:11:09 +0530

    add to git

---
+++ DiamondPattern/src/main/java/DiamondPattern.java
@@ -0,0 +1,30 @@
+
+public class DiamondPattern {
+	public void isDiamond(int n)
+	{
+		for(int i=1;i<=n-1;i++)
+		{
+			for(int j=i;j<=n;j++)
+			{
+				System.out.print(" ");
+			}
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print("*"+" ");
+			}
+			System.out.println();
+		}
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print(" ");
+			}
+			for(int j=i;j<=n;j++)
+			{
+				System.out.print("*"+" ");
+			}
+			System.out.println();
+		}
+	}
+}
---
+++ DiamondPattern/src/test/java/DiamondTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DiamondTest {
+
+	@Test
+	public void test() {
+		DiamondPattern dp=new DiamondPattern();
+		dp.isDiamond(5);
+	}
+
+}
